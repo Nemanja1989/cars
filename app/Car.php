5822c946f626cf264170e709a78de0534bb3a561
Nemanja Bojanic <nemanja.bojanic@pool4tool.com>
2017-12-21 21:13:49 +0100

    master little fun

--- app/Car.php
+++ app/Car.php
@@ -8,4 +8,10 @@ class Car extends \Eloquent
 {
     //protected $table = 'cars';
 
+    public static function getCarsWithThreeDors()
+    {
+        //pokupi ove gde je published
+        return self::where('number_od_doors', 3)->get();
+    }
+
 }
